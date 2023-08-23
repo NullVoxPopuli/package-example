e53f42bf3ac5c28635faf2f97a3c6524c7b4e78b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-22 21:48:52 -0700

    Tests pass

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
-import {  } from './sum'
+import { double } from './index'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+test('that double work', () => {
+  expect(double(3)).toBe(6)
 })
